bbe4ba7348900f7ee2a5bc3119c3ac8036d69658
Chirag D <daxinichirag26@gmail.com>
2021-08-11 18:12:22 +0100

    added reset button.

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -4,10 +4,14 @@ angular.
   module('userList').
   component('userList', {
     templateUrl: 'user-list/user-list.template.html',
-    controller: ['User',
-      function UserListController(User) {
-        this.users = User.query();
-        this.orderProp = 'Name';
+    controller: ['User','$scope', function UserListController(User, $scope) {
+      $scope.users = User.query();
+      this.orderProp = 'Name';
+        $scope.resetBalance = function () {
+          $scope.users.forEach(function(u) {
+            u.balance = 0;
+          });
+          }
       }
     ]
   });
